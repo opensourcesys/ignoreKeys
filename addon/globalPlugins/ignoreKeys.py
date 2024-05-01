2256dec8b142c8df006aea630cba6043bf94353f
Luke Davis <XLTechie+github@newanswertech.com>
2024-05-01 06:08:26 -0400

    Add the start of the GUI. It's currently detached from the functioning of the plugin.

--- addon/globalPlugins/ignoreKeys.py
+++ addon/globalPlugins/ignoreKeys.py
@@ -1,7 +1,11 @@
-# Ignore special keys
+# Ignore Keys
 # Originally a private add-on by Tyler Spivey <tspivey@pcdesk.net>, on behalf of Sarah k Alawami <marrie12@gmail.com>
 # With a subsequent rewrite and expansion by Luke Davis <XLTechie@newanswertech.com>.
 # Copyright (c) 2023-2024, Sarah k Alawami, Luke Davis, all rights reserved.
+
+import weakref
+from enum import IntFlag, auto
+from typing import Optional
 
 import config
 import gui
@@ -14,24 +18,72 @@ import winUser
 import winInputHook
 from scriptHandler import script
 from logHandler import log
-from globalCommands import SCRCAT_TOOLS
 
+try:
+	addonHandler.initTranslation()
+except addonHandler.AddonError:
+	log.error(
+		"Attempted to initialize translations in an inappropriate context. May be running from scratchpad."
+	)
 
-addonHandler.initTranslation()
-globalPluginPointer: Optional[globalPluginHandler.GlobalPlugin] = None
+# Configure the Input Gestures category
+try:
+	ADDON_SUMMARY = addonHandler.getCodeAddon().manifest["summary"]
+except Exception:
+	ADDON_SUMMARY = "Ignore Keys"  # Static assignment for emergencies (e.g. scratchpad use)
+
+pluginRef: Optional[weakref.ref[globalPluginHandler.GlobalPlugin]] = None
+"""
+Allows us a shorthand module level reference to the GlobalPlugin object once defined.
+"""
 
 config.conf.spec["ignoreSpecialKeys"] = {
-	"resetSelectedLockKeys": "boolean(default=True)",
 	"keyGroup": "integer(default=0)",
+	"restoreLockKeys": "boolean(default=True)",
+	"startInIgnoreMode": "boolean(default=False)",
 }
+
+
+class IgnoreKeysSettings (gui.settingsDialogs.SettingsPanel):
+	"""NVDA configuration panel based configurator  for Ignore Keys."""
+
+	# Translators: Label for the Ignore Keys settings category in NVDA Settings panel.
+	title: str = _("Ignore Keys")
+
+	def makeSettings(self, settingsSizer):
+		"""Creates a settings panel."""
+		helper = gui.guiHelper.BoxSizerHelper(self, sizer=settingsSizer)
+		self.startInIgnoreModeCB = helper.addItem(
+			wx.CheckBox(
+				self,
+				# Translators: The label for a checkbox in Ignore Keys settings panel
+				label=_("Start ignoring the selected keys as soon as NVDA launches")
+			)
+		)
+		self.startInIgnoreModeCB.SetValue(config.conf["ignoreKeys"]["startInIgnoreMode"])
+		self.restoreLockKeysCB = helper.addItem(
+			wx.CheckBox(
+				self,
+				# Translators: The label for a checkbox in Ignore Keys settings panel
+				label=_("Restore any selected lock keys to their original state when turning off ignore")
+			)
+		)
+		self.restoreLockKeysCB.SetValue(
+			config.conf["ignoreKeys"]["restoreLockKeys"]
+		)
+
+	def onSave(self):
+		config.conf["ignoreKeys"]["startInIgnoreMode"] = self.startInIgnoreModeCB.Value
+		config.conf["ignoreKeys"]["restoreLockKeys"] = self.restoreLockKeysCB.Value
+
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		self.patched = False
-		global globalPluginPointer
-		globalPluginPointer = self
+		self.patched: bool = False
+		global pluginRef
+		pluginRef = weakref.ref(self)
 
 	def patch(self):
 		if self.patched:
@@ -41,7 +93,7 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			keyboardHandler.internal_keyDownEvent = internal_keyDownEvent
 			winInputHook.keyDownCallback = internal_keyDownEvent
 			self.patched = True
-			tones.beep(1000, 100)
+			tones.beep(2000, 100)
 
 	def unpatch(self, shouldBeep: bool = True):
 		if self.patched:
@@ -49,15 +101,16 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			winInputHook.keyDownCallback = self.old_fn
 			self.patched = False
 			if shouldBeep:
-				tones.beep(700, 100)
+				tones.beep(1000, 100)
 
 	def terminate(self):
 		self.unpatch(False)
 
 	@script(
-		description="Toggle to keep NVDA from intercepting foot pedal keys. Press again to restore normal keyboard.",
-		gesture="kb:NVDA+shift+f8",
-		category=SCRCAT_TOOLS
+		# Translators: A description of the input gesture for Input Help.
+		description=_("Toggle to keep NVDA from intercepting the configured keys to be ignored. Press again to restore normal behavior."),
+		gesture="kb:NVDA+f8",
+		category=ADDON_SUMMARY
 	)
 	def script_patch(self, gesture):
 		if self.patched:
@@ -73,4 +126,4 @@ def internal_keyDownEvent(vkCode, scanCode, extended, injected):
 	):
 		return True
 	else:
-		return globalPluginPointer.old_fn(vkCode, scanCode, extended, injected)
+		return pluginRef.old_fn(vkCode, scanCode, extended, injected)
